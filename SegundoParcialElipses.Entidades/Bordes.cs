c4414af5167fd5d95f02d9812d59dd2280765ed6
brenda torrillo <brentorrillo@gmail.com>
2024-11-12 18:09:57 -0300

    Agregar archivos de proyecto.

---
+++ SegundoParcialElipses.Entidades/Bordes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoParcialElipses.Entidades
+{
+    public enum Bordes
+    {
+        Solido=1,
+        Punteado,
+        Rayado,
+        Doble,
+    }
+}
